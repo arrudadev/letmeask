e33161de025dd979900e272c301d2e68e8881dfb
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-06-28 23:00:09 -0300

    chore: add RoomCode component

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,27 @@
+import copyImg from '../assets/images/copy.svg';
+
+import '../styles/room-code.scss';
+
+type RoomCodeProps = {
+  code: string;
+};
+
+export function RoomCode({ code }: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(code);
+  }
+
+  return (
+    <button
+      type="button"
+      className="room-code"
+      onClick={copyRoomCodeToClipboard}
+    >
+      <div>
+        <img src={copyImg} alt="Copy room code" />
+      </div>
+
+      <span>Sala #{code}</span>
+    </button>
+  );
+}
