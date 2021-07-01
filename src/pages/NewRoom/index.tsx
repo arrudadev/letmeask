4e4a2cc4ed4aeac99b9e589c43d317674967b1f9
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-06-30 23:27:05 -0300

    refactor: moving the NewRoom page to NewRoom folder

--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,13 +1,13 @@
 import { FormEvent, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import { Button } from '../components/Button';
-import { useAuth } from '../hooks/useAuth';
-import { database } from '../services/firebase';
+import illustrationImg from '../../assets/images/illustration.svg';
+import logoImg from '../../assets/images/logo.svg';
+import { Button } from '../../components/Button';
+import { useAuth } from '../../hooks/useAuth';
+import { database } from '../../services/firebase';
 
-import '../styles/auth.scss';
+import './styles.scss';
 
 export function NewRoom() {
   const { user } = useAuth();
