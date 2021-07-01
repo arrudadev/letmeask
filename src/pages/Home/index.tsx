2a1b12311538a930e945046c1a3e4e2bb646e394
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-06-30 23:16:51 -0300

    refactor: moving the Home page to Home folder

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,14 @@
 import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import googleIconImg from '../assets/images/google-icon.svg';
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import { Button } from '../components/Button';
-import { useAuth } from '../hooks/useAuth';
-import { database } from '../services/firebase';
+import googleIconImg from '../../assets/images/google-icon.svg';
+import illustrationImg from '../../assets/images/illustration.svg';
+import logoImg from '../../assets/images/logo.svg';
+import { Button } from '../../components/Button';
+import { useAuth } from '../../hooks/useAuth';
+import { database } from '../../services/firebase';
 
-import '../styles/auth.scss';
+import './styles.scss';
 
 export function Home() {
   const history = useHistory();
