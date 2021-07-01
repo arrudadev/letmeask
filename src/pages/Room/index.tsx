b4ae78e0e8834af4ef99fdd88c4bfe60d21678c1
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-06-30 23:42:47 -0300

    refactor: moving the Room page to Room folder

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,14 +1,14 @@
 import { FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import logoImg from '../assets/images/logo.svg';
-import { Button } from '../components/Button';
-import { Question } from '../components/Question';
-import { RoomCode } from '../components/RoomCode';
-import { useAuth } from '../hooks/useAuth';
-import { database } from '../services/firebase';
+import logoImg from '../../assets/images/logo.svg';
+import { Button } from '../../components/Button';
+import { Question } from '../../components/Question';
+import { RoomCode } from '../../components/RoomCode';
+import { useAuth } from '../../hooks/useAuth';
+import { database } from '../../services/firebase';
 
-import '../styles/room.scss';
+import './styles.scss';
 
 type RoomParams = {
   id: string;
