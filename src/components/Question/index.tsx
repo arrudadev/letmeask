6b009dafc8d7b220a8ec7745ad87805e17edd272
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-07-04 18:26:58 -0300

    chore: add like button in room page

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 import './styles.scss';
 
 type QuestionProps = {
@@ -6,9 +8,10 @@ type QuestionProps = {
     avatar: string;
     name: string;
   };
+  children?: ReactNode;
 };
 
-export function Question({ content, author }: QuestionProps) {
+export function Question({ content, author, children }: QuestionProps) {
   return (
     <div className="question">
       <p>{content}</p>
@@ -18,6 +21,7 @@ export function Question({ content, author }: QuestionProps) {
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
+        <div>{children}</div>
       </footer>
     </div>
   );
