934595a589eef51bb22e6fbe59db8e2206df8abb
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2021-07-10 17:28:34 -0300

    chore: redirect admin user to admin room when join in a room

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -43,7 +43,11 @@ export function Home() {
       return;
     }
 
-    history.push(`/rooms/${roomCode}`);
+    if (roomRef.val().authorId === user?.id) {
+      history.push(`/admin/rooms/${roomCode}`);
+    } else {
+      history.push(`/rooms/${roomCode}`);
+    }
   }
 
   return (
